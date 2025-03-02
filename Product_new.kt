f540bcd29461d29ee98560f08dcecb3c9604590d
ehsansdumb <127270636+ehsansdumb@users.noreply.github.com>
2025-03-02 14:02:52 +0400

    Create Product_new.kt

---
+++ Product_new.kt
@@ -0,0 +1,8 @@
+package com.example.myauth4.models_new
+
+data class Product_new(
+    val id: String? = null,
+    val name: String? = null,
+    val price: String? = null,
+    val imgUri: String? = null
+)
